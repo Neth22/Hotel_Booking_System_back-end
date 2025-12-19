d36db41bc972790fee70c5fe05f9932ad67cb887
Neth22 <nseneviratne44@gmail.com>
2025-12-19 11:45:57 +0530

    Add review API and integrate with hotel model. Implement review creation and retrieval functionality. Update hotel API to handle reviews and refactor hotel data management to use MongoDB.
    Signed-off-by: Neth22 <nseneviratne44@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import hotelsRouter from "./api/hotel.js";
+import reviewRouter from "./api/review.js";
 import connectDB from "./infrastructure/db.js";
 import "dotenv/config";
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/hotels", hotelsRouter);
+app.use("/api/reviews", reviewRouter);
 
 connectDB();
 
